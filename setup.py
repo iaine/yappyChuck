c0e576ac3bdb4f787bb605ed21f62fe0c8aa629b
Iain Emsley <I.Emsley@sussex.ac.uk>
2024-03-31 22:23:31 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='0.1',
+      description='yappyChuck',
+      author='Iain Emsley',
+      author_email='iain.emsley@warwick.ac.uk',
+      url='https://iaine.github.io',
+      packages=['yappyChuck'],
+     )
