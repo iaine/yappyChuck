138bace11b18550bed931c368cc4f9e0816c2d79
iaine <iain.emsley@kellogg.ox.ac.uk>
2025-09-03 10:37:41 +0100

    update yappy functions and added messaging patterns

--- yappyChuck.py
+++ yappyChuck.py
@@ -14,9 +14,12 @@ class Server():
         self.port = port_number
         self.process = None
 
-    def start(self):
+    def start(self, command_string):
         try:
-            self.process = subprocess.Popen(["chuck","server.ck"], shell=False)
+            cwd = os.getcwd()
+
+            self.process = subprocess.Popen(["`which chuck` {}".format(cwd + "/sound/" + command_string)], shell=True, \
+                                        stderr=subprocess.PIPE, stdout= subprocess.PIPE)
             if not self.process.pid:
                 raise Exception('No server')
             print(self.process.pid)
@@ -25,6 +28,7 @@ class Server():
 
     def stop(self):
         pid =  self.process.pid
+
         self.process.terminate()
 
         #let's catch any zombies
@@ -37,26 +41,29 @@ class Server():
 
 class Client():
 
-    def __init__(self) -> None:
-        self.host = '127.0.0.1'
-        self.port = 5005
+    def __init__(self, host, port) -> None:
+        self.host = host
+        self.port = port
 
-    def send(self, message):
+    def send_once(self, channel, message) -> None:
         '''
            Function to send the message
         '''
         try:
             client = udp_client.SimpleUDPClient(self.host, self.port)
-            client.send_message("/debug", message)
+            client.send_message("/{}".format(channel), message)
         except Exception as e:
             print("Client Exception {}".format(e))
 
-if __name__ == "__main__":
-    server = Server(5005)
-    print(server.port)
-    server.start()
-
-    client = Client()
-    client.send("edge:261.25:0.5:3:0.25")
-
-    server.stop()
+    def request_response(self, channel, message) -> bytes:
+        '''
+           Function to send the message and receive a response
+        '''
+        recv_msg = ""
+        try:
+            client = udp_client.SimpleUDPClient(self.host, self.port)
+            client.send_message("/{}".format(channel), message)
+            recv_msg = client.receive("/{}".format(channel))
+        except Exception as e:
+            print("Client Exception {}".format(e))
+        return recv_msg
